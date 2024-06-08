4eec01776a356cebf27674f5fee30bd990318413
noofny <noofny@gmail.com>
2024-06-08 17:19:49 +1000

    ...

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ function cleanup() {
 TEMP_FOLDER_PATH=$(mktemp -d)
 pushd $TEMP_FOLDER_PATH >/dev/null
 
-echo '>>>> V3.5 <<<<<'
+echo '>>>> V3.6 <<<<<'
 
 # prompts/args
 DEFAULT_HOSTNAME='photos-2'
